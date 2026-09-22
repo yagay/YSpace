cb0fc4c12ee9af6563cc95d5f32fbc4af54d049e
Jie <1614094+yagay@users.noreply.github.com>
2026-09-22 08:00:47 +0100

    Use libxposed service 101 for SDK 36 dynamic scope support

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,5 +34,5 @@ android {
 
 dependencies {
     compileOnly("io.github.libxposed:api:102.0.0")
-    implementation("io.github.libxposed:service:102.0.0")
+    implementation("io.github.libxposed:service:101.0.0")
 }
