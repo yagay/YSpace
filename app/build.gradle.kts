a86e6c6b8fe46a30879cadb38e5e618b57f203d2
Jie <1614094+yagay@users.noreply.github.com>
2026-09-22 07:40:19 +0100

    Bump YSpace to 0.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.yagay.YSpace"
         minSdk = 28
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.2.0"
     }
 
     compileOptions {
